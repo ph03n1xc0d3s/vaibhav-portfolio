e853ecada1ab954c89e849220b01ac165df45ca2
ph03n1x <ph03n1xc0d3s@gmail.com>
2024-07-15 02:17:28 +0530

    adsa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
-import '../assets/css/navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../assets/css/navbar.css";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className='navbar-container'>
-            <nav className='navbar'>
-                <div className="logo-image">
-                    <h2 className='name'>Vaibhav M</h2>
-                </div>
-                <div className="menu-toggle" onClick={toggleMenu}>
-                    <div className={`hamburger ${isOpen ? 'open' : ''}`}></div>
-                </div>
-                <ul className={`unordered-list ${isOpen ? 'open' : ''}`}>
-                    <li className="list">
-                        <a href="/">Home</a>
-                        <a href="/projects">Projects</a>
-                        <a href="/journey">Journey</a>
-                        <a href="/contact">Contact</a>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <div className="navbar-container">
+      <nav className="navbar">
+        <div className="logo-image">
+          <h2 className="name">Vaibhav M</h2>
         </div>
-    );
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className={`hamburger ${isOpen ? "open" : ""}`}></div>
+        </div>
+        <ul className={`unordered-list ${isOpen ? "open" : ""}`}>
+          <li className="list">
+            <Link to="/">
+              Home
+            </Link>
+            <Link to="/projects">
+              Projects
+            </Link>
+            <Link to="/journey">
+              Journey
+            </Link>
+            <Link to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
